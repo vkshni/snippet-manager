354399ca7cbf1a90cbdc23a2a615990ad8b848a2
vkshni <vksdrive24@gmail.com>
2026-03-18 21:25:09 +0530

    generate_hash

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 # Utilities
 from datetime import datetime
-from storage import CounterFile
+import hashlib
+from counter_storage import CounterFile
 
 
 COUNTER_DATE_FORMAT = "%d%m%Y"
@@ -12,7 +13,7 @@ counter = CounterFile()
 def generate_snippet_id():
 
     current_date = datetime.now().strftime(COUNTER_DATE_FORMAT)
-    counter_data = counter.json_handler.read_all()
+    counter_data = counter.read_all()
 
     if current_date == counter_data["last_date"]:
         next_id = counter_data["last_id"] + 1
@@ -22,8 +23,17 @@ def generate_snippet_id():
         next_id = 1
         counter_data["last_id"] = next_id
 
-    counter.json_handler.write_all(counter_data)
+    counter.write_all(counter_data)
     return f"{current_date}_{next_id:05d}"
 
 
-# print(generate_snippet_id())
+# generate sha256 hash
+def generate_hash(password_str):
+
+    password_hash = hashlib.sha256(password_str.encode()).hexdigest()
+    return password_hash
+
+
+if __name__ == "__main__":
+    print(generate_snippet_id())
+    # print(generate_hash("vks123"))
