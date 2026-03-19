5a9f55a963648d3d6f4646268164e085a3ab4974
vkshni <vksdrive24@gmail.com>
2026-03-19 21:15:47 +0530

    methods

--- snippet_manager.py
+++ snippet_manager.py
@@ -8,17 +8,42 @@ class SnippetManager:
     def __init__(self, SNIPPET_FILE="snippets.json"):
         self.snippet_db = SnippetDB(SNIPPET_FILE)
 
-    def add_snippet(self, title, content, tag=None):
+    def add_snippet(self, title, content, tag=None, access_level="PUBLIC"):
 
-        snippet_obj = Snippet(title, content, tag)
+        snippet_obj = Snippet(title.strip(), content, tag, access_level=access_level)
 
         self.snippet_db.add_snippet(snippet_obj)
-        return True
+        return snippet_obj
 
-    def get_snippet_by_title(self, title):
+    def list_all(self, status="ACTIVE"):
 
         snippets = self.snippet_db.get_all()
-        filtered = [s for s in snippets if s.title.lower() == title.lower()]
+        filtered = [s for s in snippets if s.status == status]
+        return filtered
+
+    def list_by_tag(self, tag=None, status="ACTIVE"):
+
+        snippets = self.snippet_db.get_all()
+        filtered = [
+            s for s in snippets if s.tag == tag.lower() and s.status == status.upper()
+        ]
+        return filtered
+
+    def get_snippet_by_title(self, title, status="ACTIVE"):
+
+        snippets = self.snippet_db.get_all()
+        if not snippets:
+            raise ValueError("No snippets found")
+
+        filtered = [
+            s
+            for s in snippets
+            if s.title.lower() == title.lower() and s.status == status
+        ]
+
+        if not filtered:
+            raise ValueError(f"Snippet with titel '{title}' not found")
+
         return filtered
 
 
